f7da359b49c752404ec2055c8fa3ba30b296bdda
Abe Kor <60723388+abe1kor2@users.noreply.github.com>
2025-11-29 00:44:56 -0330

    updated home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,30 @@
 import { ThemeToggle } from "../components/ThemeToogle";
+import { StartBackground } from "../components/StartBackground";
+import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
+import { AboutMeSection } from "../components/AboutMeSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ProjectSection } from "../components/ProjectSection";
 
 export const Home = () => {
-    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <ThemeToggle />
+      {/* background effects */}
+      <StartBackground />
 
-        <ThemeToggle />
-        {/* background effects */}
+      {/* navbar */}
+      <Navbar />
 
-        {/* navbar */}
+      {/* main content */}
+      <main>
+        <HeroSection />
+        <AboutMeSection />
+        <SkillsSection />
+        <ProjectSection />
+      </main>
 
-        {/* main content */}
-
-        {/* footer */}
-    </div>;
-}
+      {/* footer */}
+    </div>
+  );
+};
